d7782b7275ddfe95f7d00e7de3a093bdf3ff54b2
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-06-06 20:13:37 -0300

    Recoded Alive fix
    Ygoproco/Live2017Links@c2434a3f00ccb5ce2270462b129424f40198f65c

--- script/c70238111.lua
+++ script/c70238111.lua
@@ -1,13 +1,12 @@
 --リコーデッド・アライブ
---Recoded Alive
---Scripted by Eerie Code
 function c70238111.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
 	e1:SetTarget(c70238111.target)
 	e1:SetOperation(c70238111.activate)
 	c:RegisterEffect(e1)
@@ -18,32 +17,27 @@ function c70238111.initial_effect(c)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetHintTiming(0,TIMING_END_PHASE)
 	e2:SetCondition(c70238111.spcon)
 	e2:SetCost(c70238111.spcost)
 	e2:SetTarget(c70238111.sptg)
 	e2:SetOperation(c70238111.spop)
 	c:RegisterEffect(e2)
 end
-function c70238111.filter(c)
+function c70238111.filter(c,tp)
 	return c:IsRace(RACE_CYBERSE) and c:GetLink()==3 and (c:IsFaceup() or not c:IsLocation(LOCATION_MZONE))
-		and c:IsAbleToRemove()
+		and c:IsAbleToRemove() and Duel.GetLocationCountFromEx(tp,tp,c)>0
 end
 function c70238111.spfilter(c,e,tp)
 	return c:IsSetCard(0x101) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c70238111.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(e:GetLabel()) and chkc:IsControler(tp) and c70238111.filter(chkc) end
-	if chk==0 then
-		local ft=Duel.GetLocationCountFromEx(tp)
-		if ft<-1 then return false end
-		local loc=LOCATION_GRAVE+LOCATION_MZONE
-		if ft==0 then loc=LOCATION_MZONE end
-		e:SetLabel(loc)
-		return Duel.IsExistingTarget(c70238111.filter,tp,loc,0,1,nil)
-			and Duel.IsExistingMatchingCard(c70238111.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_MZONE) and chkc:IsControler(tp) and c70238111.filter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(c70238111.filter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,nil,tp)
+		and Duel.IsExistingMatchingCard(c70238111.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp)
 	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectTarget(tp,c70238111.filter,tp,e:GetLabel(),0,1,1,nil)
+	local g=Duel.SelectTarget(tp,c70238111.filter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,1,nil,tp)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
@@ -58,22 +52,25 @@ function c70238111.activate(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c70238111.spcfilter(c)
+function c70238111.cfilter(c)
 	return c:GetSequence()>=5
 end
 function c70238111.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return not Duel.IsExistingMatchingCard(c70238111.spcfilter,tp,LOCATION_MZONE,0,1,nil)
+	return not Duel.IsExistingMatchingCard(c70238111.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function c70238111.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
+function c70238111.spfilter2(c,e,tp)
+	return c:IsFaceup() and c:IsSetCard(0x101) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
 function c70238111.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and c70238111.spfilter(chkc,e,tp) end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and c70238111.spfilter2(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c70238111.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(c70238111.spfilter2,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c70238111.spfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c70238111.spfilter2,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c70238111.spop(e,tp,eg,ep,ev,re,r,rp)
