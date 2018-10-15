7a904b526727313275d09e52b3fdab131d590559
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-10-15 10:19:38 -0300

    Script synch with Ygopro Percy (14 Oct 2018)
    Official passcodes
    Bug fixes
    Removal of obsolete scripts

--- script/c4638410.lua
+++ script/c4638410.lua
@@ -16,10 +16,14 @@ function c4638410.initial_effect(c)
 	e2:SetValue(c4638410.efilter)
 	c:RegisterEffect(e2)
 end
+function c4638410.cfilter(c,ft,tp)
+	return ft>0 or (c:IsControler(tp) and c:GetSequence()<5)
+end
 function c4638410.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroup(tp,nil,1,nil) end
-	local rg=Duel.SelectReleaseGroup(tp,nil,1,1,nil)
-	Duel.Release(rg,REASON_COST)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return ft>-1 and Duel.CheckReleaseGroupCost(tp,c4638410.cfilter,1,false,nil,nil,ft,tp) end
+	local g=Duel.SelectReleaseGroupCost(tp,c4638410.cfilter,1,1,false,nil,nil,ft,tp)
+	Duel.Release(g,REASON_COST)
 end
 function c4638410.etarget(e,c)
 	return c:GetOwner()==e:GetHandlerPlayer()
