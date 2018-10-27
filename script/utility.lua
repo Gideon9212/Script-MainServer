3a17d2f4a7fbed8b6bc32fe0447a2dd59046a9bb
edo9300 <edoardo762@gmail.com>
2018-10-27 16:09:02 +0200

    Added bit functions

--- script/utility.lua
+++ script/utility.lua
@@ -11,6 +11,42 @@ RACE_DIVINE=RACE_DEVINE
 SUMMON_TYPE_TRIBUTE=SUMMON_TYPE_ADVANCE
 SUMMON_TYPE_GEMINI=SUMMON_TYPE_DUAL
 
+--the lua version of the bit32 lib, which is deprecated in lua 5.3
+bit={}
+function bit.band(a,b)
+    return a&b
+end
+function bit.bor(a,b)
+    return a|b
+end
+function bit.bxor(a,b)
+    return a~b
+end
+function bit.lshift(a,b)
+    return a<<b
+end
+function bit.rshift(a,b)
+    return a>>b
+end
+function bit.bnot(a)
+    return ~a
+end
+local function fieldargs(f,width)
+    w=width or 1
+    assert(f>=0,"field cannot be negative")
+    assert(w>0,"width must be positive")
+    assert(f+w<=32,"trying to access non-existent bits")
+    return f,~(-1<<w)
+end
+function bit.extract(r,field,width)
+    local f,m=fieldargs(field,width)
+    return (r>>f)&m
+end
+function bit.replace(r,v,field,width)
+    local f,m=fieldargs(field,width)
+    return (r&~(m<<f))|((v&m)<< f)
+end
+
 Group.__band = function (o1,o2)
 	if userdatatype(o1)~="Group" then o1,o2=o2,o1 end
 	o1=o1:Clone()
