25c5f422d13353ad5661e23bd9b828954fc024ed
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-10-28 09:06:56 -0300

    Fixed "Impcantation Chalislime"

--- script/c65877963.lua
+++ script/c65877963.lua
@@ -1,18 +1,18 @@
 --
 --Impcantation Chalislime
 --Scripted by Logical Nonsense
-function c81524756.initial_effect(c)
+function c65877963.initial_effect(c)
 	c:EnableReviveLimit()
 	--Special summon from deck
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.String65877963(65877963,0))
+	e1:SetDescription(aux.Stringid(65877963,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCost(c81524756.spcost)
+	e1:SetCost(c65877963.spcost)
 	e1:SetCountLimit(1,65877963)
-	e1:SetTarget(c81524756.sptg)
-	e1:SetOperation(c81524756.spop)
+	e1:SetTarget(c65877963.sptg)
+	e1:SetOperation(c65877963.spop)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_DESTROY)
@@ -20,33 +20,33 @@ function c81524756.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,65877963)
-	e2:SetCost(c81524756.cost)
-	e2:SetTarget(c81524756.target)
-	e2:SetOperation(c81524756.operation)
+	e2:SetCost(c65877963.cost)
+	e2:SetTarget(c65877963.target)
+	e2:SetOperation(c65877963.operation)
 	c:RegisterEffect(e2)
 end
-function c81524756.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c65877963.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return not e:GetHandler():IsPublic() end
 end
-function c81524756.spfilter(c,e,tp)
+function c65877963.spfilter(c,e,tp)
 	return c:IsSetCard(0x117) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c81524756.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c65877963.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c81524756.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c65877963.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function c81524756.spop(e,tp,eg,ep,ev,re,r,rp)
+function c65877963.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT+REASON_DISCARD)==0 then return end
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c81524756.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c65877963.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-		e1:SetOperation(c81524756.regop)
+		e1:SetOperation(c65877963.regop)
 		e1:SetLabel(1)
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1,tp)
@@ -54,43 +54,43 @@ function c81524756.spop(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e2:SetCode(EVENT_PHASE+PHASE_END)
 		e2:SetCountLimit(1)
-		e2:SetCondition(c81524756.damcon)
-		e2:SetOperation(c81524756.damop)
+		e2:SetCondition(c65877963.damcon)
+		e2:SetOperation(c65877963.damop)
 		e2:SetReset(RESET_PHASE+PHASE_END)
 		e2:SetLabelObject(e1)
 		Duel.RegisterEffect(e2,tp)
 	end
 end
-function c81524756.regop(e,tp,eg,ep,ev,re,r,rp)
+function c65877963.regop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==0 then return end
 	if eg and eg:IsExists(Card.IsSummonType,1,nil,SUMMON_TYPE_RITUAL) then
 		e:SetLabel(0)
 	end
 end
-function c81524756.damcon(e,tp,eg,ep,ev,re,r,rp)
+function c65877963.damcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetLabelObject():GetLabel()~=0
 end
-function c81524756.damop(e,tp,eg,ep,ev,re,r,rp)
+function c65877963.damop(e,tp,eg,ep,ev,re,r,rp)
 	local lp=Duel.GetLP(tp)
 	Duel.SetLP(tp,lp-2500)
 end
-function c81524756.costfilter(c)
+function c65877963.costfilter(c)
 	return c:IsSetCard(0x117) and (c:IsLocation(LOCATION_HAND) or c:IsFaceup()) and c:IsAbleToGraveAsCost()
 end
-function c81524756.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c81524756.costfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
+function c65877963.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c65877963.costfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c81524756.costfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c65877963.costfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
 	Duel.SendtoGrave(g,REASON_COST)
 end
-function c81524756.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c65877963.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
 	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c81524756.operation(e,tp,eg,ep,ev,re,r,rp)
+function c65877963.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
