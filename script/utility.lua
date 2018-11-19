c1393c6006d6428b6a113b89fce2c6084e176d44
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-11-19 09:13:42 -0200

    Utilities synch with Ygopro Percy 19 Nov 2018

--- script/utility.lua
+++ script/utility.lua
@@ -662,6 +662,14 @@ end
 function Auxiliary.tgoval(e,re,rp)
 	return rp~=e:GetHandlerPlayer()
 end
+--filter for EFFECT_INDESTRUCTABLE_EFFECT + self
+function Auxiliary.indsval(e,re,rp)
+    return rp==e:GetHandlerPlayer()
+end
+--filter for EFFECT_INDESTRUCTABLE_EFFECT + opponent
+function Auxiliary.indoval(e,re,rp)
+    return rp==1-e:GetHandlerPlayer()
+end
 --filter for non-zero ATK 
 function Auxiliary.nzatk(c)
 	return c:IsFaceup() and c:GetAttack()>0
@@ -1243,6 +1251,60 @@ function Duel.GetZoneWithLinkedCount(count,tp)
 		end
 	end
 	return rzone
+end
+
+--Checks whether a card has an effect that can add a certain type of counter
+function aux.IsCounterAdded(c,counter)
+	if not c.counter_add_list then return false end
+	for _,ccounter in ipairs(c.counter_add_list) do
+		if counter==ccounter then return true end
+	end
+	return false
+end
+
+--Help functions for the Salamangreats' effects
+function Card.IsReincarnationSummoned(c)
+	return c:GetFlagEffect(CARD_SALAMANGREAT_SANCTUARY)~=0
+end
+function Auxiliary.EnableCheckReincarnation(c)
+	local m=_G["c"..CARD_SALAMANGREAT_SANCTUARY]
+	if not m then
+		m=_G["c"..c:GetCode()]
+	end
+	if m and not m.global_check then
+		m.global_check=true
+        local e1=Effect.GlobalEffect()
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_MATERIAL_CHECK)
+        e1:SetValue(Auxiliary.ReincarnationCheckValue)
+        local ge1=Effect.GlobalEffect()
+        ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+        ge1:SetLabelObject(e1)
+        ge1:SetTargetRange(0xff,0xff)
+        ge1:SetTarget(Auxiliary.ReincarnationCheckTarget)
+        Duel.RegisterEffect(ge1,0)
+	end
+end
+function Auxiliary.ReincarnationCheckTarget(e,c)
+	return c:IsType(TYPE_FUSION+TYPE_RITUAL+TYPE_LINK)
+end
+function Auxiliary.ReincarnationRitualFilter(c,id,tp)
+	return c:IsCode(id) and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
+end
+function Auxiliary.ReincarnationCheckValue(e,c)
+	local g=c:GetMaterial()
+	local id=c:GetCode()
+	local rc=false
+	if c:IsType(TYPE_LINK) then
+		rc=g:IsExists(Card.IsLinkCode,1,nil,id)
+	elseif c:IsType(TYPE_FUSION) then
+		rc=g:IsExists(Card.IsFusionCode,1,nil,id)
+	elseif c:IsType(TYPE_RITUAL) then
+		rc=g:IsExists(Aux.ReincarnationRitualFilter,1,nil,id,c:GetControler())
+	end
+	if rc then
+		c:RegisterFlagEffect(CARD_SALAMANGREAT_SANCTUARY,RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD-RESET_LEAVE-RESET_TEMP_REMOVE,0,1)
+	end
 end
 
 function loadutility(file)
