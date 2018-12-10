320a927eff08f3a874cab3f308909da70f0689e3
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-12-10 10:26:45 -0200

    Synch with percy (up to 09 Dec 2018)

--- script/c63504681.lua
+++ script/c63504681.lua
@@ -1,24 +1,26 @@
 --No.86 H－C ロンゴミアント
-function c63504681.initial_effect(c)
+--Number 86: Heroic Champion - Rhongomyniad
+local s,id=GetID()
+function s.initial_effect(c)
 	--xyz summon
 	aux.AddXyzProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_WARRIOR),4,2,nil,nil,5)
 	c:EnableReviveLimit()
 	--remove material
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(63504681,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_PHASE+PHASE_END)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(c63504681.rmcon)
-	e1:SetOperation(c63504681.rmop)
+	e1:SetCondition(s.rmcon)
+	e1:SetOperation(s.rmop)
 	c:RegisterEffect(e1)
 	--battle indestructable
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e2:SetValue(1)
-	e2:SetCondition(c63504681.effcon)
+	e2:SetCondition(s.effcon)
 	e2:SetLabel(1)
 	c:RegisterEffect(e2)
 	--atk/def
@@ -28,7 +30,7 @@ function c63504681.initial_effect(c)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCode(EFFECT_UPDATE_ATTACK)
 	e3:SetValue(1500)
-	e3:SetCondition(c63504681.effcon)
+	e3:SetCondition(s.effcon)
 	e3:SetLabel(2)
 	c:RegisterEffect(e3)
 	local e4=e3:Clone()
@@ -40,8 +42,8 @@ function c63504681.initial_effect(c)
 	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e5:SetRange(LOCATION_MZONE)
 	e5:SetCode(EFFECT_IMMUNE_EFFECT)
-	e5:SetValue(c63504681.efilter)
-	e5:SetCondition(c63504681.effcon)
+	e5:SetValue(s.efilter)
+	--e5:SetCondition(s.effcon) --handled in Value for mid-resolution updating
 	e5:SetLabel(3)
 	c:RegisterEffect(e5)
 	--disable spsummon
@@ -51,7 +53,7 @@ function c63504681.initial_effect(c)
 	e6:SetCode(EFFECT_CANNOT_SUMMON)
 	e6:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e6:SetTargetRange(0,1)
-	e6:SetCondition(c63504681.effcon)
+	e6:SetCondition(s.effcon)
 	e6:SetLabel(4)
 	c:RegisterEffect(e6)
 	local e7=e6:Clone()
@@ -59,39 +61,39 @@ function c63504681.initial_effect(c)
 	c:RegisterEffect(e7)
 	--destroy
 	local e8=Effect.CreateEffect(c)
-	e8:SetDescription(aux.Stringid(63504681,1))
+	e8:SetDescription(aux.Stringid(id,1))
 	e8:SetCategory(CATEGORY_DESTROY)
 	e8:SetType(EFFECT_TYPE_IGNITION)
 	e8:SetCountLimit(1)
 	e8:SetRange(LOCATION_MZONE)
-	e8:SetCondition(c63504681.effcon)
-	e8:SetTarget(c63504681.destg)
-	e8:SetOperation(c63504681.desop)
+	e8:SetCondition(s.effcon)
+	e8:SetTarget(s.destg)
+	e8:SetOperation(s.desop)
 	e8:SetLabel(5)
 	c:RegisterEffect(e8)
 end
-c63504681.xyz_number=86
-function c63504681.rmcon(e,tp,eg,ep,ev,re,r,rp)
+s.xyz_number=86
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()~=tp
 end
-function c63504681.rmop(e,tp,eg,ep,ev,re,r,rp)
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:GetOverlayCount()>0 then
 		c:RemoveOverlayCard(tp,1,1,REASON_EFFECT)
 	end
 end
-function c63504681.effcon(e,tp,eg,ep,ev,re,r,rp)
+function s.effcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetOverlayCount()>=e:GetLabel()
 end
-function c63504681.efilter(e,te)
-	return te:GetOwner()~=e:GetOwner()
+function s.efilter(e,te)
+	return te:GetOwner()~=e:GetOwner() and s.effcon(e) --condition handling for mid-resolution updating
 end
-function c63504681.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
-function c63504681.desop(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
 	Duel.Destroy(g,REASON_EFFECT)
 end
